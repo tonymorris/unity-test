8502341b5df677c49ec502ca18b94d6b203f3a8c
Tony Morris <tmorris@tmorris.net>
2017-05-14 15:53:44 +1000

    hai

---
+++ Assets/clock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clock : MonoBehaviour {
+
+  IEnumerator Tick() {
+
+  }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
