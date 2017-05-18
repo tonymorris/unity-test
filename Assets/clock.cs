0c522c6597063c081dd2044718abf931ad10a889
Adam Morris <amorris2003@gmail.com>
2017-05-18 18:07:59 +1000

    Ticking both sides

--- Assets/clock.cs
+++ Assets/clock.cs
@@ -5,16 +5,50 @@ using UnityEngine.UI;
 
 public class clock : MonoBehaviour {
 
-	public int time = 600;
-	public Text text;
+	public int timeWhite = 600;
+	public int timeBlack = 600;
+	public Text textWhite;
+	public Text textBlack;
+	public Button button;
+	public bool white = true;
 
-	void Tick() {
-		time--;
-		text.text = (time / 60).ToString() + ":" + (time % 60).ToString();
+	void WhiteTick() {
+		timeWhite--;
+		textWhite.text = (timeWhite / 60).ToString() + ":" + (timeWhite % 60).ToString();
 	}
 
-	public void Start() {
-		InvokeRepeating ("Tick", 1, 1);
+	void BlackTick() {
+		timeBlack--;
+		textBlack.text = (timeBlack / 60).ToString() + ":" + (timeBlack % 60).ToString();
+	}
+
+	public void click() {
+
+		ColorBlock cb = button.colors;
+
+		if (white) {
+			CancelInvoke ();
+			InvokeRepeating ("WhiteTick", 1, 1);
+			white = false;
+			cb.normalColor = Color.black;
+			cb.highlightedColor = Color.black;
+			cb.pressedColor = Color.black;
+			cb.disabledColor = Color.black;
+			button.colors = cb;
+			textWhite.color = Color.white;
+			textBlack.color = Color.white;
+		} else {
+			CancelInvoke ();
+			InvokeRepeating ("BlackTick", 1, 1);
+			white = true;
+			cb.normalColor = Color.white;
+			cb.highlightedColor = Color.white;
+			cb.pressedColor = Color.white;
+			cb.disabledColor = Color.white;
+			button.colors = cb;
+			textWhite.color = Color.black;
+			textBlack.color = Color.black;
+		}
 	}
 
 }
